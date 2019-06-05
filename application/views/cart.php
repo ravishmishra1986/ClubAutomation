0328a03b95b46cdcf6790272ae864bb5ffb6f532
Ravish <ravish.varanasi@gmail.com>
2019-06-05 23:59:50 +0530

    Commit UI changes

--- application/views/cart.php
+++ application/views/cart.php
@@ -1,17 +1,18 @@
 <script src="<?php echo $this->config->item('url_frontend') . '/assets/js/jquery-1.9.1.min.js'; ?>"></script>
+<link rel="stylesheet" href="<?php echo $this->config->item('url_frontend') . 'assets/css/cart.css'; ?>">
 <div style="max-width:1000px; margin: auto auto; position: relative;">
-    <div style="float:left; width: 100%; background: #2e8ece">
+    <div class="header-div">
         <div style="float: left; padding: 5px 0px 5px 5px;">
             <a href="<?php echo $this->config->item('url_frontend'); ?>"><img src="<?php echo $this->config->item('url_frontend') . '/assets/images/logo.png'; ?>"></a>
         </div>
         <div style="float: right; padding: 32px 5px 28px 0px; width: 40%;">
-            <div style="float: right; padding: 20px 0px">
-                <button type="button" class="btn btn-primary btn-sm"  id= "back">Back</button>
+            <div style="float: right;>
+                <button type="button" class="button"  id= "back">Back</button>
             </div>
         </div>
     </div>
 
-    <div class="list-left">
+    <div class="list-data">
         <?php if (!empty($errors)) { ?>
             <?php foreach ($errors as $error) { ?>
                 <div style="float: left; width: 100%;"> <?php echo $error['error']; ?></div>
@@ -22,7 +23,7 @@
                     Products</a></div>
         <?php }
         else { ?>
-        <table border="1" cellspacing="0" width="100%">
+        <table cellspacing="0" class="table">
             <thead>
             <tr>
                 <th>S. No.</th>
--- application/views/product_listing.php
+++ application/views/product_listing.php
@@ -1,43 +1,44 @@
-<script src="<?php echo $this->config->item('url_frontend') . '/assets/js/jquery-1.9.1.min.js'; ?>"></script>
+<script src="<?php echo $this->config->item('url_frontend') . 'assets/js/jquery-1.9.1.min.js'; ?>"></script>
+<link rel="stylesheet" href="<?php echo $this->config->item('url_frontend') . 'assets/css/cart.css'; ?>">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript">
     var baseUrl = '<?php echo $this->config->item('url_frontend'); ?>';
     var addToCartUrl = '<?php echo $this->config->item('url_frontend') . 'products/addToCart'?>';
 </script>
 <div id="cover-spin" style="display: none;"></div>
 <div style="max-width:1000px; margin: auto auto; position: relative;">
-    <div style="float:left; width: 100%; background: #2e8ece">
+    <div class="header-div">
         <div style="float: left; padding: 5px 0px 5px 5px;">
             <a href="<?php echo $this->config->item('url_frontend'); ?>"><img
-                        src="<?php echo $this->config->item('url_frontend') . '/assets/images/logo.png'; ?>"></a>
+                        src="<?php echo $this->config->item('url_frontend') . 'assets/images/logo.png'; ?>"></a>
         </div>
-        <div style="float: right; padding: 32px 5px 28px 0px; width: 40%;">
+        <div class="search-div">
             <div style="width: 70%; float: left">
-                <span>Search Ingredients</span>
+                <!--<span>Search Ingredients</span>-->
                 <span>
                     <form method="get" name="search_ingredient" action="">
-                        <span><input type="text" name="key" style="width: 150px;" placeholder="For example: bread"
+                        <span><input type="text" name="key" placeholder="Search Ingredients..." class="form-control"
                                      value="<?php echo (isset($data['key']) && !empty($data['key'])) ? $data['key'] : ''; ?>"
                                      style="" id="key"></span>
-                        <span><input type="submit" id="find_ingredient"></span>
+                        <span><input type="submit" value="Search" id="find_ingredient" class="button"></span>
                     </form>
                 </span>
             </div>
-            <div style="float: right; padding: 20px 0px">
-                <a href="<?php echo $this->config->item('url_frontend') . 'products/cart'; ?>">Cart(<span
-                            id="cart_count"><?php echo (isset($data['cart_count']) && !empty($data['cart_count'])) ? $data['cart_count'] : 0; ?></span>)</a>
+            <div class="cart-link">
+                <a href="<?php echo $this->config->item('url_frontend') . 'products/cart'; ?>" style="position: relative;"><i class="fa fa-shopping-cart" style="font-size:24px"></i><div id="cart_count" class="cart-item-cnt"><?php echo (isset($data['cart_count']) && !empty($data['cart_count'])) ? $data['cart_count'] : 0; ?></div><span> Cart</span></a>
             </div>
         </div>
     </div>
-    <div class="list-left">
+    <div class="list-data">
         <?php if (!empty($data['response']['errors'])) { ?>
             <?php foreach ($data['response']['errors']['error'] as $error) { ?>
-                <div style="float: left; width: 100%;"> <?php echo $error['message']; ?></div>
+                <div class="message-div"> <?php echo $error['message']; ?></div>
             <?php }
         } elseif (empty($data['response'])) { ?>
-            <div style="float: left; width: 100%;">No Result found. Please search your product.</div>
+            <div class="message-div">No Result found. Please search your product.</div>
         <?php }
         else { ?>
-        <table border="1" cellspacing="0" width="100%">
+        <table cellspacing="0" class="table">
             <thead>
             <tr>
                 <th>Action</th>
@@ -48,7 +49,7 @@
                 if (!empty($value['name'])) {
                     ?>
                     <tr>
-                        <td><input type="button" id="<?php echo $value['ndbno']; ?>" class="add_to_cart_link"
+                        <td><input type="button" id="<?php echo $value['ndbno']; ?>" class="add_to_cart_link button"
                                    value="Add to Cart">
                         </td>
                         <td><?php echo $value['name']; ?></td>
@@ -75,7 +76,7 @@
                     success: function (data) {
                         $("#cover-spin").hide();
                         console.log(data);
-                        if (data.status == 1) {
+                        if (data.status) {
                             $('#cart_count').html(data.cart_count);
                             //alert(data.message)
                         } else {
@@ -88,19 +89,3 @@
         });
     })
 </script>
-<style type="text/css">
-    #cover-spin {
-        position: absolute;
-        top: 0px;
-        right: 0px;
-        width: 100%;
-        height: 100%;
-        background-color: #666;
-        background-image: url("<?php echo $this->config->item('url_frontend') . '/assets/images/loader.gif'; ?>");
-        background-repeat: no-repeat;
-        background-position: center;
-        z-index: 10000000;
-        opacity: 0.4;
-        filter: alpha(opacity=40); /* For IE8 and earlier */
-    }
-</style>
